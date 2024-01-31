17fe099884536a480715a42decf08e9c724fbe69
Ignacio Biran <nqchossj@gmail.com>
2024-01-31 04:13:41 -0300

    update link styles can be added

--- src/components/LinkUI.tsx
+++ src/components/LinkUI.tsx
@@ -3,11 +3,15 @@ import Link from "next/link";
 interface LinkUIProps {
   children: React.ReactNode;
   href: string;
+  style?: string;
 }
 
-export const LinkUI = ({ children, href }: LinkUIProps) => {
+export const LinkUI = ({ children, href, style }: LinkUIProps) => {
   return (
-    <Link href={href} className="text-sm hover:opacity-75 transition-all">
+    <Link
+      href={href}
+      className={`text-sm hover:opacity-75 transition-all truncate ${style}`}
+    >
       {children}
     </Link>
   );
