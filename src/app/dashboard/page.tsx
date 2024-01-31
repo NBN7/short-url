56fc73b4959df829e7ce80f225ce85f644502b32
Ignacio Biran <nqchossj@gmail.com>
2024-01-31 04:24:08 -0300

    update check user session

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,25 @@
 "use client";
 
+import { useEffect } from "react";
+
+import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 
 import { AboveTheFold } from "@/components/AboveTheFold";
 import { Dashboard } from "@/components/Dashboard";
 
+import { ROUTES } from "@/constants/routes";
+
 export default function DashboardPage() {
   const { data: session } = useSession();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!session) {
+      router.push(ROUTES.HOME);
+    }
+  }, [session]);
 
   const props = {
     title: "Dashboard",
