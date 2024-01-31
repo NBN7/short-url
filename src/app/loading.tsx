309380c94a9e53eb008707fff3669407b8223067
Ignacio Biran <nqchossj@gmail.com>
2024-01-31 04:12:23 -0300

    update loading page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import { CircularProgress } from "@nextui-org/progress";
 
 export default function Loading() {
   return (
-    <main className="w-full h-screen flex items-center justify-center">
+    <main className="w-full h-[calc(100vh-65px)] flex items-center justify-center">
       <CircularProgress color="default" size="sm" aria-label="Loading.." />
     </main>
   );
