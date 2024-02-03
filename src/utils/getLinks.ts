2c166c1086669b97f6a9922011d8de4b20982684
Ignacio Biran <nqchossj@gmail.com>
2024-02-03 18:15:45 -0300

    update get links function

--- src/utils/getLinks.ts
+++ src/utils/getLinks.ts
@@ -1,10 +1,8 @@
 import type { Session } from "next-auth";
 import type { Link } from "@prisma/client";
 
-export const getLinks = async (
-  session: Session | null
-): Promise<Link[] | void> => {
-  if (!session) return;
+export const getLinks = async (session: Session | null): Promise<Link[] | null> => {
+  if (!session) return null;
 
   const res = await fetch(`/api/urls/${session.user?.email}`);
   const data = await res.json();
