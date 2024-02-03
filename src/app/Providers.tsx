44655a7ff0ff4839f3c2521f2259f78f8392ee88
Ignacio Biran <nqchossj@gmail.com>
2024-02-03 16:50:43 -0300

    add react query

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,12 +1,17 @@
 "use client";
 
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { NextUIProvider } from "@nextui-org/react";
 import { SessionProvider } from "next-auth/react";
 
+const queryClient = new QueryClient();
+
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <NextUIProvider>
-      <SessionProvider>{children}</SessionProvider>
-    </NextUIProvider>
+    <QueryClientProvider client={queryClient}>
+      <NextUIProvider>
+        <SessionProvider>{children}</SessionProvider>
+      </NextUIProvider>
+    </QueryClientProvider>
   );
 }
