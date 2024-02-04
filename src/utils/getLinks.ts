dc455658cd4f4d01b76095e7f451d5d6ef8bf4e9
Ignacio Biran <nqchossj@gmail.com>
2024-02-04 20:37:50 -0300

    update try catch

--- src/utils/getLinks.ts
+++ src/utils/getLinks.ts
@@ -1,11 +1,21 @@
 import type { Session } from "next-auth";
 import type { Link } from "@prisma/client";
 
-export const getLinks = async (session: Session | null): Promise<Link[] | null> => {
-  if (!session) return null;
+export const getLinks = async (
+  session: Session | null
+): Promise<Link[] | null> => {
+  try {
+    if (!session) return null;
 
-  const res = await fetch(`/api/urls/${session.user?.email}`);
-  const data = await res.json();
+    const res = await fetch(`/api/urls/${session.user?.email}`);
+    const data = await res.json();
 
-  return data;
+    return data;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    } else {
+      throw new Error("An unknown error occurred");
+    }
+  }
 };
