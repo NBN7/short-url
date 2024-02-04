b1de89bb1aec7d24193671b7a64c58b35e419b09
Ignacio Biran <nqchossj@gmail.com>
2024-02-04 20:38:15 -0300

    update function

--- src/utils/copyToClipboard.ts
+++ src/utils/copyToClipboard.ts
@@ -1,11 +1,11 @@
-import { toastSuccess, toastError } from "./toastNotifications";
-
 export const copyToClipboard = async (text: string) => {
   try {
     await navigator.clipboard.writeText(text);
-
-    toastSuccess("Copied to clipboard");
   } catch (error) {
-    toastError("Failed to copy to clipboard");
+    if (error instanceof Error && error.message) {
+      throw new Error(error.message);
+    } else {
+      throw new Error("An unknown error occurred");
+    }
   }
 };
