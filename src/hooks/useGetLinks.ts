9ae159686adc9efbc10a41d9874d23dd5b8d9c89
Ignacio Biran <nqchossj@gmail.com>
2024-02-05 19:59:37 -0300

    update use links custom hook

---
+++ src/hooks/useGetLinks.ts
@@ -0,0 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { getLinks } from "@/utils/getLinks";
+
+import type { Session } from "next-auth";
+
+interface useLinksProps {
+  session: Session | null;
+}
+
+export const useGetLinks = ({ session }: useLinksProps) => {
+  const {
+    isLoading,
+    isError,
+    data: links,
+    refetch,
+  } = useQuery({
+    queryKey: ["links"],
+    queryFn: () => getLinks({ session }),
+    enabled: !!session,
+  });
+
+  return {
+    isLoading,
+    isError,
+    links,
+    refetch,
+  };
+};
