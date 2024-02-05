b1ec26a5b5b005dd63a96bd27a469006df6380af
Ignacio Biran <nqchossj@gmail.com>
2024-02-05 19:58:50 -0300

    add redirect function

---
+++ src/utils/redirect.ts
@@ -0,0 +1,25 @@
+interface RedirectProps {
+  shortUrl: string | string[];
+}
+
+export const redirect = async ({ shortUrl }: RedirectProps) => {
+  try {
+    const res = await fetch(`/api/urls/${shortUrl}`, {
+      mode: "no-cors",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+    const data = await res.json();
+
+    window.location.href = data.url;
+
+    return data;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    } else {
+      throw new Error("An unknown error occurred");
+    }
+  }
+};
