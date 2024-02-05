401f75b4b556e09209d8ad8c0f72b0dd35f3cef2
Ignacio Biran <nqchossj@gmail.com>
2024-02-05 19:59:05 -0300

    add redirect custom hook

---
+++ src/hooks/useRedirect.ts
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { redirect } from "@/utils/redirect";
+
+interface useRedirectProps {
+  shortUrl: string | string[];
+}
+
+export const useRedirect = ({ shortUrl }: useRedirectProps) => {
+  const {
+    isLoading,
+    isError,
+    data: link,
+  } = useQuery({
+    queryKey: ["redirect", shortUrl],
+    queryFn: () => redirect({ shortUrl }),
+  });
+
+  return {
+    isLoading,
+    isError,
+    link,
+  };
+};
