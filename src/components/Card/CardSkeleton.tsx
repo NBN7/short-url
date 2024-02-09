c41863331e9cc8dbac13991029ad7ff055fb8b6b
Ignacio Biran <nqchossj@gmail.com>
2024-02-08 23:34:43 -0300

    add card folder component

---
+++ src/components/Card/CardSkeleton.tsx
@@ -0,0 +1,5 @@
+import { Skeleton } from "@nextui-org/skeleton";
+
+export const CardSkeleton = () => {
+  return <Skeleton className="w-full h-[100px] rounded-xl" />;
+};
---
+++ src/components/Card/index.tsx
@@ -0,0 +1,36 @@
+import { CardDropdown } from "./CardDropdown";
+import { LinkUI } from "../LinkUI";
+
+import { Card as CardUI, CardHeader } from "@nextui-org/card";
+
+import { ROUTES } from "@/constants/routes";
+
+import type { Link } from "@prisma/client";
+import type { Session } from "next-auth";
+
+interface CardProps {
+  link: Link;
+  session: Session | null;
+}
+
+const Card = ({ link, session }: CardProps) => {
+  return (
+    <CardUI key={link.id}>
+      <CardHeader className="justify-between">
+        <div className="overflow-hidden">
+          <LinkUI href={`${ROUTES.REDIRECT}/${link.shortUrl}`}>
+            {ROUTES.REDIRECT}/{link.shortUrl}
+          </LinkUI>
+          <p className="text-gray-500 truncate">{link.url}</p>
+          <p className="text-sm text-gray-400 mt-2 truncate">
+            {link.description}
+          </p>
+        </div>
+
+        <CardDropdown session={session} link={link} />
+      </CardHeader>
+    </CardUI>
+  );
+};
+
+export default Card;
