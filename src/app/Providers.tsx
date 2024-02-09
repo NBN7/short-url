567ddd8f30365dab9bbbcfad13ced296a4c9a7b3
Ignacio Biran <nqchossj@gmail.com>
2024-02-08 22:59:00 -0300

    add next nprogress bar

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -4,6 +4,7 @@ import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { NextUIProvider } from "@nextui-org/react";
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "react-hot-toast";
+import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
 
 const queryClient = new QueryClient();
 
@@ -12,6 +13,13 @@ export function Providers({ children }: { children: React.ReactNode }) {
     <QueryClientProvider client={queryClient}>
       <NextUIProvider>
         <SessionProvider>
+          <ProgressBar
+            color="#ffffff"
+            height="2px"
+            options={{
+              showSpinner: false,
+            }}
+          />
           {children}
           <Toaster />
         </SessionProvider>
