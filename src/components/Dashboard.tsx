200ce7e33f7e6f68af2fb00eef922f1534cd7032
Ignacio Biran <nqchossj@gmail.com>
2024-02-08 23:59:05 -0300

    update component

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -4,6 +4,7 @@ import { useGetLinks } from "@/hooks/useGetLinks";
 
 const Card = lazy(() => import("./Card/index"));
 import { CardSkeleton } from "./Card/CardSkeleton";
+import { CardEmpty } from "./Card/CardEmpty";
 
 import type { Link } from "@prisma/client";
 import type { Session } from "next-auth";
@@ -20,7 +21,7 @@ export const Dashboard = ({ session }: DashboardProps) => {
   return (
     <section className="w-full flex flex-col gap-6 mt-6">
       {links?.length === 0 ? (
-        <></>
+        <CardEmpty />
       ) : (
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-3">
           {reversedLinks?.map((link: Link) => (
