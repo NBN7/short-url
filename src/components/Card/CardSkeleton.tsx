c4576db5d90249688af5b713cbbcb1163198ecf4
Ignacio Biran <nqchossj@gmail.com>
2024-02-08 23:59:39 -0300

    update card skeleton height

--- src/components/Card/CardSkeleton.tsx
+++ src/components/Card/CardSkeleton.tsx
@@ -1,5 +1,5 @@
 import { Skeleton } from "@nextui-org/skeleton";
 
 export const CardSkeleton = () => {
-  return <Skeleton className="w-full h-[100px] rounded-xl" />;
+  return <Skeleton className="w-full h-[104px] rounded-xl" />;
 };
