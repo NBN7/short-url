0083fbb5a185cfb47d783594645dc205c572d4ad
Ignacio Biran <nqchossj@gmail.com>
2024-02-08 23:34:20 -0300

    update lazy suspense

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,14 +1,11 @@
+import { Suspense, lazy } from "react";
+
 import { useGetLinks } from "@/hooks/useGetLinks";
 
-import { LinkUI } from "./LinkUI";
-import { CardDropdown } from "./CardDropdown";
-
-import { Card, CardHeader } from "@nextui-org/card";
-
-import { ROUTES } from "@/constants/routes";
+const Card = lazy(() => import("./Card/index"));
+import { CardSkeleton } from "./Card/CardSkeleton";
 
 import type { Link } from "@prisma/client";
-
 import type { Session } from "next-auth";
 
 interface DashboardProps {
@@ -23,29 +20,13 @@ export const Dashboard = ({ session }: DashboardProps) => {
   return (
     <section className="w-full flex flex-col gap-6 mt-6">
       {links?.length === 0 ? (
-        <Card>
-          <CardHeader>
-            <p className="font-semibold text-gray-400">No links to show</p>
-          </CardHeader>
-        </Card>
+        <></>
       ) : (
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-3">
           {reversedLinks?.map((link: Link) => (
-            <Card key={link.id}>
-              <CardHeader className="justify-between">
-                <div className="overflow-hidden">
-                  <LinkUI href={`${ROUTES.REDIRECT}/${link.shortUrl}`}>
-                    {ROUTES.REDIRECT}/{link.shortUrl}
-                  </LinkUI>
-                  <p className="text-gray-500 truncate">{link.url}</p>
-                  <p className="text-sm text-gray-400 mt-2 truncate">
-                    {link.description}
-                  </p>
-                </div>
-
-                <CardDropdown session={session} link={link} />
-              </CardHeader>
-            </Card>
+            <Suspense key={link.id} fallback={<CardSkeleton />}>
+              <Card link={link} session={session} />
+            </Suspense>
           ))}
         </div>
       )}
